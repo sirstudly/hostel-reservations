c0a76e1b16d8eb9e33c65469d4cf017d205cd331
urinal.puck@gmail.com <urinal.puck@gmail.com@29b641af-e9ce-2339-0548-842034013da8>
2012-06-06 17:53:30 +0000

    initial attempt to display allocation date data

--- trunk/wordpress/wp-content/plugins/booking/include/allocation_row.class.php
+++ trunk/wordpress/wp-content/plugins/booking/include/allocation_row.class.php
@@ -8,6 +8,8 @@ class AllocationRow {
     var $name;
     var $gender;
     var $resource;
+    var $showMinDate;   // minimum date to show on the table
+    var $showMaxDate;   // maximum date to show on the table
     private $bookingDatePayment = array();  // key = booking date, value = payment amount
 
     function AllocationRow($name, $gender, $resource) {
@@ -16,8 +18,27 @@ class AllocationRow {
         $this->resource = $resource;
     }
     
+    /**
+     * Adds a date/payment entry for this allocation row
+     * $dt  date string in format dd.MM.yyyy
+     * $payment  value of payment for specified date
+     */
     function addPaymentForDate($dt, $payment) {
         $this->bookingDatePayment[$dt] = $payment;
+    }
+    
+    /**
+     * Returns the minimum date (DateTime) for this allocation.
+     */
+    function getMinDate() {
+        $result = null;
+        foreach ($this->bookingDatePayment as $bookingDate => $payment) {
+            $bd = DateTime::createFromFormat('!d.m.Y', $bookingDate, new DateTimeZone('UTC'));
+            if($result == null || $bd < $result) {
+                $result = $bd;
+            }
+        }
+        return $result;
     }
     
     /**
@@ -51,11 +72,37 @@ class AllocationRow {
         $attrTotal = $domtree->createAttribute('total');
         $attrTotal->value = $this->getTotalPayment();
         $dateRow->appendChild($attrTotal);
-        foreach ($this->bookingDatePayment as $bookingDate => $payment) {
-            $dateElem = $dateRow->appendChild($domtree->createElement('date', $bookingDate));
-            $attrPayment = $domtree->createAttribute('payment');
-            $attrPayment->value = $payment;
-            $dateElem->appendChild($attrPayment);
+        
+        if($this->showMinDate != null && $this->showMaxDate != null) {
+        
+            // loop from showMinDate to showMaxDate creating a date element for every day in between
+            // set the appropriate state if a booking exists for that date
+            $dt = clone $this->showMinDate;
+            while ($dt < $this->showMaxDate) {
+                $dateElem = $dateRow->appendChild($domtree->createElement('date', $dt->format('d.m.Y')));
+                $payment = $this->bookingDatePayment[$dt->format('d.m.Y')];
+                if($payment != null) {   // also means that booking exists on this date
+                    $attrPayment = $domtree->createAttribute('payment');
+                    $attrPayment->value = $payment;
+                    $dateElem->appendChild($attrPayment);
+    
+                    $attrState = $domtree->createAttribute('state');
+                    $attrState->value = 'pending';
+                    $dateElem->appendChild($attrState);
+                
+                // TODO: different state when date is in the past?
+
+                } else { // booking doesn't exist
+                    $attrPayment = $domtree->createAttribute('payment');
+                    $attrPayment->value = 15;  // TODO: add payment rules
+                    $dateElem->appendChild($attrPayment);
+                    
+                    $attrState = $domtree->createAttribute('state');
+                    $attrState->value = 'inactive';
+                    $dateElem->appendChild($attrState);
+                }
+                $dt->add(new DateInterval('P1D'));  // increment by day
+            }
         }
         $xmlRoot->appendChild($dateRow);
     }
--- trunk/wordpress/wp-content/plugins/booking/include/allocation_table.class.php
+++ trunk/wordpress/wp-content/plugins/booking/include/allocation_table.class.php
@@ -13,9 +13,38 @@ class AllocationTable {
         $this->bookingName = $bookingName;
     }
 
-    function addAllocation($numVisitors, $gender, $resourceId) {
+    function addAllocation($numVisitors, $gender, $resourceId, $dates) {
+        $datearr = explode(",", $dates);
         for($i = 0; $i < $numVisitors; $i++) {
-            $this->allocationRows[] = new AllocationRow($this->bookingName.'-'.(sizeof($this->allocationRows)+1), $gender, $resourceId);
+            $allocationRow = new AllocationRow($this->bookingName.'-'.(sizeof($this->allocationRows)+1), $gender, $resourceId);
+            foreach ($datearr as $dt) {
+                $allocationRow->addPaymentForDate(trim($dt), 15); // FIXME: price fixed at 15
+            }
+            $this->allocationRows[] = $allocationRow;
+        }
+    }
+    
+    /**
+     * This will set the showMinDate, showMaxDate properties to their default values.
+     * showMinDate will be set to 3 days prior to the minimum date on allocationRows
+     * showMaxDate will be set to 14 days after showMinDate
+     */
+    function setDefaultMinMaxDates() {
+        $result = null; 
+        
+        // first find the min date
+        foreach ($this->allocationRows as $allocation) {
+            $minRowDate = $allocation->getMinDate();
+            if($result == null || $minRowDate < $result) {
+                $result = $minRowDate;
+            }
+        }
+        
+        if($result != null) {
+            $result->sub(new DateInterval('P3D'));  // default to 3 days prior
+            $this->showMinDate = $result;
+            $this->showMaxDate = clone $result;
+            $this->showMaxDate->add(new DateInterval('P14D'));  // default to 14 days after
         }
     }
     
@@ -35,10 +64,22 @@ class AllocationTable {
       Generates the following xml:
         <allocations total="49.50">
             <bookingName>Megan</bookingName>
-            <showMinDate>15.08.2012</showMinDate>
-            <showMaxDate>25.08.2012</showMaxDate>
+            <showMinDate>25.08.2012</showMinDate>
+            <showMaxDate>04.09.2012</showMaxDate>
             <allocation>...</allocation>
             <allocation>...</allocation>
+            <dateheaders>
+                <header>August/September</header>
+                <datecol>
+                    <date>25</date>
+                    <day>Sun</day>
+                <datecol>
+                <datecol>
+                    <date>26</date>
+                    <day>Mon</day>
+                <datecol>
+                ...
+            </dateheaders>
         </allocations>
      */
     function toXml() {
@@ -50,14 +91,40 @@ class AllocationTable {
         $xmlRoot = $domtree->appendChild($xmlRoot);
     
         $xmlRoot->appendChild($domtree->createElement('bookingName', $this->bookingName));
-        $xmlRoot->appendChild($domtree->createElement('showMinDate', $this->showMinDate));
-        $xmlRoot->appendChild($domtree->createElement('showMaxDate', $this->showMaxDate));
+        if($this->showMinDate != null) {
+            $xmlRoot->appendChild($domtree->createElement('showMinDate', $this->showMinDate->format('d.m.Y')));
+        }
+        if($this->showMaxDate != null) {
+            $xmlRoot->appendChild($domtree->createElement('showMaxDate', $this->showMaxDate->format('d.m.Y')));
+        }
 
         $attrTotal = $domtree->createAttribute('total');
         $attrTotal->value = $this->getTotalPayment();
         $xmlRoot->appendChild($attrTotal);
         foreach ($this->allocationRows as $allocation) {
+            $allocation->showMinDate = $this->showMinDate;
+            $allocation->showMaxDate = $this->showMaxDate;
             $allocation->addSelfToDocument($domtree, $xmlRoot);
+        }
+
+        // build dateheaders to be used to display availability table
+        if($this->showMinDate != null && $this->showMaxDate != null) {
+            $dateHeaders = $xmlRoot->appendChild($domtree->createElement('dateheaders'));
+            
+            // if spanning more than one month, print out both months
+            if($this->showMinDate->format('F') !== $this->showMaxDate->format('F')) {
+                $dateHeaders->appendChild($domtree->createElement('header', $this->showMinDate->format('F') . '/' . $this->showMaxDate->format('F')));
+            } else {
+                $dateHeaders->appendChild($domtree->createElement('header', $this->showMinDate->format('F')));
+            }
+            
+            $dt = clone $this->showMinDate;
+            while ($dt < $this->showMaxDate) {
+                $dateElem = $dateHeaders->appendChild($domtree->createElement('datecol'));
+                $dateElem->appendChild($domtree->createElement('date', $dt->format('d')));
+                $dateElem->appendChild($domtree->createElement('day', $dt->format('D')));
+                $dt->add(new DateInterval('P1D'));  // increment by day
+            }
         }
         return $domtree->saveXML();
     }
