c4b079be9ff6312164b17d71b0c504d6c8639e73
sirstudly <ron1404@techie.com>
2017-02-09 22:50:12 +0000

    force login for all reports which include individual booking data

--- wordpress/wp-content/plugins/booking/templates/guest-comments.php
+++ wordpress/wp-content/plugins/booking/templates/guest-comments.php
@@ -1,24 +1,29 @@
 <?php 
 
-    // use the same page controller if already defined
-    if (isset($_SESSION['GUEST_COMMENTS_CONTROLLER'])) {
-        $rep = $_SESSION['GUEST_COMMENTS_CONTROLLER'];
-    }
-    else {
-        $rep = new LHGuestCommentsReport();
-        $_SESSION['GUEST_COMMENTS_CONTROLLER'] = $rep;
-    }
+    // user must be logged in to access this page
+    if ( false === is_user_logged_in() ) { 
+        header( 'Location: '.home_url()."/wp-login.php?redirect_to=".urlencode($_SERVER['REQUEST_URI']) ) ;
 
-    if (isset($_POST['reload_data'])) {
-        $rep->submitReportJob();
-        wp_redirect( get_permalink() ); // redirect after POST to avoid resubmissions
-        exit;
-    }
-    else {
-        get_header();
-        $rep->doView(); // update the view
-        echo $rep->toHtml();
-        get_footer(); 
-    }
+    } else {
+        // use the same page controller if already defined
+        if (isset($_SESSION['GUEST_COMMENTS_CONTROLLER'])) {
+            $rep = $_SESSION['GUEST_COMMENTS_CONTROLLER'];
+        }
+        else {
+            $rep = new LHGuestCommentsReport();
+            $_SESSION['GUEST_COMMENTS_CONTROLLER'] = $rep;
+        }
 
+        if (isset($_POST['reload_data'])) {
+            $rep->submitReportJob();
+            wp_redirect( get_permalink() ); // redirect after POST to avoid resubmissions
+            exit;
+        }
+        else {
+            get_header();
+            $rep->doView(); // update the view
+            echo $rep->toHtml();
+            get_footer(); 
+        }
+    }
 ?>
