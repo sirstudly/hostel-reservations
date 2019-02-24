b226fd069a03d4786baabad9e90333429595c03c
sirstudly <ron1404@techie.com>
2019-02-24 01:12:04 +0000

    fixed directory links in copy_hbo-reports.php.sh

--- bin/copy_hbo-reports.php.sh
+++ bin/copy_hbo-reports.php.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-cp -p wordpress/wp-content/plugins/hbo-reports/hbo-reports.php ../castlerock/wp-content/plugins/hbo-reports/
-cp -p wordpress/wp-content/plugins/hbo-reports/hbo-reports.php ../highstreethostel/wp-content/plugins/hbo-reports/
-cp -p wordpress/wp-content/plugins/hbo-reports/hbo-reports.php ../royalmile/wp-content/plugins/hbo-reports/
-cp -p wordpress/wp-content/plugins/hbo-reports/hbo-reports.php ../fortwilly/wp-content/plugins/hbo-reports/
+PROJECT_ROOT=/var/www/vhosts/macbackpackers.com/backoffice.scotlandstophostels.com
+cp -p $PROJECT_ROOT/wordpress/wp-content/plugins/hbo-reports/hbo-reports.php $PROJECT_ROOT/castlerock/wp-content/plugins/hbo-reports/
+cp -p $PROJECT_ROOT/wordpress/wp-content/plugins/hbo-reports/hbo-reports.php $PROJECT_ROOT/highstreethostel/wp-content/plugins/hbo-reports/
+cp -p $PROJECT_ROOT/wordpress/wp-content/plugins/hbo-reports/hbo-reports.php $PROJECT_ROOT/royalmile/wp-content/plugins/hbo-reports/
+cp -p $PROJECT_ROOT/wordpress/wp-content/plugins/hbo-reports/hbo-reports.php $PROJECT_ROOT/fortwilly/wp-content/plugins/hbo-reports/
