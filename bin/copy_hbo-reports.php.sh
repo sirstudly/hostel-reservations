461d6bf70e65a95378404fcd279d30f7ef2a2698
sirstudly <ron1404@techie.com>
2019-02-24 01:46:32 +0000

    fix bin scripts

--- bin/copy_hbo-reports.php.sh
+++ bin/copy_hbo-reports.php.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+# run with sudo please!
 PROJECT_ROOT=/var/www/vhosts/macbackpackers.com/backoffice.scotlandstophostels.com
-cp -p $PROJECT_ROOT/wordpress/wp-content/plugins/hbo-reports/hbo-reports.php $PROJECT_ROOT/castlerock/wp-content/plugins/hbo-reports/
-cp -p $PROJECT_ROOT/wordpress/wp-content/plugins/hbo-reports/hbo-reports.php $PROJECT_ROOT/highstreethostel/wp-content/plugins/hbo-reports/
-cp -p $PROJECT_ROOT/wordpress/wp-content/plugins/hbo-reports/hbo-reports.php $PROJECT_ROOT/royalmile/wp-content/plugins/hbo-reports/
-cp -p $PROJECT_ROOT/wordpress/wp-content/plugins/hbo-reports/hbo-reports.php $PROJECT_ROOT/fortwilly/wp-content/plugins/hbo-reports/
+cp -p $PROJECT_ROOT/common/wordpress/wp-content/plugins/hbo-reports/hbo-reports.php $PROJECT_ROOT/castlerock/wp-content/plugins/hbo-reports/
+cp -p $PROJECT_ROOT/common/wordpress/wp-content/plugins/hbo-reports/hbo-reports.php $PROJECT_ROOT/highstreethostel/wp-content/plugins/hbo-reports/
+cp -p $PROJECT_ROOT/common/wordpress/wp-content/plugins/hbo-reports/hbo-reports.php $PROJECT_ROOT/royalmile/wp-content/plugins/hbo-reports/
+cp -p $PROJECT_ROOT/common/wordpress/wp-content/plugins/hbo-reports/hbo-reports.php $PROJECT_ROOT/fortwilly/wp-content/plugins/hbo-reports/
