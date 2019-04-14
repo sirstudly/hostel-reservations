9f815adea41c5af2a1d109f7a42a3a04512821f9
sirstudly <ron1404@techie.com>
2019-04-14 01:19:22 +0100

    Re-enabled Charge Deposits job

--- wordpress/wp-content/plugins/hbo-reports/include/scheduled_job.class.php
+++ wordpress/wp-content/plugins/hbo-reports/include/scheduled_job.class.php
@@ -9,19 +9,24 @@ abstract class ScheduledJob {
      * Returns map of classname -> user friendly name
      */
     static function getClassnameMap() {
-        return array(
-//            'com.macbackpackers.jobs.ScrapeReservationsBookedOnJob' => 'Confirm Hostelworld Deposits',
+        $result = array();
+        if( 'cloudbeds' != get_option('hbo_property_manager')) {
+            // little hotelier specific
+            $result['com.macbackpackers.jobs.ScrapeReservationsBookedOnJob'] = 'Confirm Hostelworld Deposits';
+        }
+        $result = array_merge( $result, array(
             'com.macbackpackers.jobs.HousekeepingJob' => 'Update Housekeeping Report',
             'com.macbackpackers.jobs.AllocationScraperJob' => 'Update Booking Reports',
             'com.macbackpackers.jobs.BedCountJob' => 'Update Bed Counts',
             'com.macbackpackers.jobs.DbPurgeJob' => 'Purge Old Database Entries',
-//            'com.macbackpackers.jobs.CreateDepositChargeJob' => 'Charge Deposits',
+            'com.macbackpackers.jobs.CreateDepositChargeJob' => 'Charge Deposits',
             'com.macbackpackers.jobs.CreatePrepaidChargeJob' => 'Charge Pre-Paid Bookings',
             'com.macbackpackers.jobs.CreateAgodaChargeJob' => 'Charge Past Agoda Bookings',
             'com.macbackpackers.jobs.CreateChargeHostelworldLateCancellationJob' => 'Charge HWL Late Cancellations',
             'com.macbackpackers.jobs.CreateCopyCardDetailsToCloudbedsJob' => 'Copy Card Details from Hostelworld',
             'com.macbackpackers.jobs.CreateChargeNonRefundableBookingJob' => 'Charge Non-Refundable Bookings'
-        );
+        ));
+        return $result;
     }
 
     /**
