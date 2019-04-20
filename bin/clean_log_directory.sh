b865d3d3774b37092aa4cf15778ae74c9612f2ed
sirstudly <ron1404@techie.com>
2019-04-21 00:41:54 +0100

    added lochside

--- bin/clean_log_directory.sh
+++ bin/clean_log_directory.sh
@@ -3,3 +3,4 @@ find /var/www/vhosts/macbackpackers.com/backoffice.scotlandstophostels.com/castl
 find /var/www/vhosts/macbackpackers.com/backoffice.scotlandstophostels.com/highstreethostel/logs -regextype posix-extended -regex '.*(log|gz)' -type f -mtime +60 -delete
 find /var/www/vhosts/macbackpackers.com/backoffice.scotlandstophostels.com/royalmile/logs -regextype posix-extended -regex '.*(log|gz)' -type f -mtime +60 -delete
 find /var/www/vhosts/macbackpackers.com/backoffice.scotlandstophostels.com/fortwilly/logs -regextype posix-extended -regex '.*(log|gz)' -type f -mtime +30 -delete
+find /var/www/vhosts/macbackpackers.com/backoffice.scotlandstophostels.com/lochside/logs -regextype posix-extended -regex '.*(log|gz)' -type f -mtime +30 -delete
